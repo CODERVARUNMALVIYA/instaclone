b60545e4d44ac50afd4036bbd36e33c9ac16938f
Varun Malviya <varunmalviya738@gmail.com>
2025-08-10 17:20:08 +0530

    atlas add

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const mongoose =  require('mongoose');
 const plm = require('passport-local-mongoose')
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/instadata');
+mongoose.connect('mongodb+srv://varunmalviya333:ZTb9RJTONRdGEnjW@cluster0.elknylu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 const userSchema = mongoose.Schema({
 username : String,
